bba387b20b644fd4cd154b2e41f413995f866fc1
Галина Макенова <makenova11@mail.ru>
2022-08-25 13:45:22 +0700

    Контекст

---
+++ Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskManager.Models
+{
+    public partial class Task
+    {
+        public long TaskCode { get; set; }
+        public string Task1 { get; set; } = null!;
+        public DateOnly Date { get; set; }
+        public bool IsDeleted { get; set; }
+        public long Id { get; set; }
+
+        public virtual User IdNavigation { get; set; } = null!;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskManager.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        public long Id { get; set; }
+        public string SecondName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public long Salt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
