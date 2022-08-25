8b0717f75f56d0b767942759d6fd520d2b343d6b
Галина Макенова <makenova11@mail.ru>
2022-08-25 14:40:35 +0700

    added log and context files

--- Models/TaskManagerContext.cs
+++ Models/TaskManagerContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace TaskManager.Models
 {
@@ -16,15 +13,16 @@ namespace TaskManager.Models
         {
         }
 
+        private readonly IConfiguration _configuration;
         public virtual DbSet<Task> Tasks { get; set; } = null!;
+
         public virtual DbSet<User> Users { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Database=TaskManager;Username=postgres;Password=postgres");
+                optionsBuilder.UseNpgsql(_configuration["ConnectionStrings:WebApiDatabase"]);
             }
         }
 
---
+++ Settings/LogSettings.cs
@@ -0,0 +1,47 @@
+﻿namespace TaskManager.Settings
+{
+    using System;
+
+    using Microsoft.Extensions.Configuration;
+
+    using Serilog;
+    using Serilog.Core;
+    using Serilog.Debugging;
+
+    /// <summary>
+    ///   Settings of logging.
+    /// </summary>
+    internal static class LogSetting
+    {
+        /// <summary>
+        ///   Set Serilog.
+        /// </summary>
+        /// <param name="configuration"> Configuration with serilog settings. </param>
+        /// <returns> Logger. </returns>
+        public static ILogger SetSerilog(IConfigurationRoot configuration)
+        {
+            Log.Logger = GetLogger(configuration);
+
+            SelfLog.Enable(s => Console.WriteLine($"Internal Error with Serilog: {s}"));
+
+            return Log.Logger;
+        }
+
+        /// <summary>
+        ///   Logger building.
+        /// </summary>
+        /// <param name="configuration"> Configuration with serilog settings. </param>
+        /// <returns> Logger. </returns>
+        private static Logger GetLogger(IConfiguration configuration)
+        {
+            var loggerConfiguration = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(configuration);
+            loggerConfiguration.WriteTo.File("log.txt", fileSizeLimitBytes: null);
+
+            return new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
