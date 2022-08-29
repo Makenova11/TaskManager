2743d7c123ac41410fa8f7e74c3dee684c7e17cd
Галина Макенова <makenova11@mail.ru>
2022-08-29 11:30:00 +0700

    Добавлены интерфейсы репозиториев

---
+++ Interfaces/IEntityBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace TaskManager.Interfaces
+{
+    /// <summary>
+    /// Базовый репозиторий.
+    /// </summary>
+    public interface IEntityBaseRepository<T> where T : class, new()
+    {
+        public Task<IEnumerable<T>> AllIncludingAsync(
+          params Expression<Func<T, object>>[] includeProperties
+        );
+        public Task<IReadOnlyList<T>> GetAllAsync();
+        public Task<int> CountAsync();
+        public Task<T> GetFirstAsync(Expression<Func<T, bool>> predicate);
+        public Task<T> GetFirstAsync(Expression<Func<T, bool>> predicate,
+          params Expression<Func<T, object>>[] includeProperties);
+        public Task<IEnumerable<T>> FindByAsync(Expression<Func<T, bool>> predicate);
+        public Task AddTaskAsync(T entity);
+        public Task UpdateTaskAsync(T entity);
+        public Task DeleteTaskAsync(T entity);
+        public Task DeleteWhereAsync(Expression<Func<T, bool>> predicate);
+        public Task SaveAsync();
+    }
+}
---
+++ Interfaces/ITaskService.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Interfaces
+{
+    /// <summary>
+    /// Сервис по работе с задачами.
+    /// </summary>
+    public interface ITaskService
+    {
+        public Task<string> Get(string id);
+    }
+}
